6997e07891cc89369ec9a06662282bbe2b520621
Mike Johnston <mrbean355@gmail.com>
2019-07-17 08:05:00 +0200

    Convert Dagger module to a library

---
+++ app/src/main/java/com/github/mrbean355/android/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.github.mrbean355.android
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mrbean355.android.dagger2.ui.ViewHeroesActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        action_dagger_2.setOnClickListener {
+            startActivity(Intent(this, ViewHeroesActivity::class.java))
+        }
+    }
+}
