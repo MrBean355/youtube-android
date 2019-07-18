a519dcab20110ae5bd6cf1e49fcb45304dabf63f
Mike Johnston <mrbean355@gmail.com>
2019-07-18 10:10:53 +0200

    Update bottom nav sample
    Use setupActionBarWithNavController() so the activity's title is set to the current destination's label.

--- navigation/src/main/java/com/github/mrbean355/android/navigation/bottomnav/BottomNavActivity.kt
+++ navigation/src/main/java/com/github/mrbean355/android/navigation/bottomnav/BottomNavActivity.kt
@@ -3,6 +3,8 @@ package com.github.mrbean355.android.navigation.bottomnav
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.github.mrbean355.android.navigation.R
 import kotlinx.android.synthetic.main.activity_bottom_nav.*
@@ -15,5 +17,8 @@ class BottomNavActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         bottom_nav_view.setupWithNavController(navController)
+
+        // This will ensure the toolbar's title is set to the current destination's label:
+        setupActionBarWithNavController(navController, AppBarConfiguration(bottom_nav_view.menu))
     }
 }
