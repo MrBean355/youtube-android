c39c7c8b34ccc9268415abefaaac07c13031fb15
Mike Johnston <mrbean355@gmail.com>
2019-10-13 09:44:38 +0200

    Convert from AsyncTask to Coroutines

--- view-model/src/main/java/com/github/mrbean355/android/viewmodel/ViewPokemonViewModel.kt
+++ view-model/src/main/java/com/github/mrbean355/android/viewmodel/ViewPokemonViewModel.kt
@@ -1,26 +1,21 @@
 package com.github.mrbean355.android.viewmodel
 
-import android.os.AsyncTask
 import com.github.mrbean355.android.framework.Pokemon
 import com.github.mrbean355.android.framework.PokemonRepository
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ViewPokemonViewModel {
 
     fun initialise(onSuccess: (items: List<Pokemon>) -> Unit) {
-        GetPokemonTask(onSuccess).execute()
-    }
-
-    private class GetPokemonTask(private val onSuccess: (items: List<Pokemon>) -> Unit)
-        : AsyncTask<Void, Void, List<Pokemon>>() {
-
-        private val repository = PokemonRepository()
-
-        override fun doInBackground(vararg params: Void?): List<Pokemon> {
-            return repository.getAll()
-        }
-
-        override fun onPostExecute(result: List<Pokemon>) {
-            onSuccess(result)
+        GlobalScope.launch(context = IO) {
+            val data = PokemonRepository().getAll()
+            withContext(Main) {
+                onSuccess(data)
+            }
         }
     }
 }
