2bb1eb4a6ecd196a028f2e19e4da8ed621c48b8f
Mike Johnston <mrbean355@gmail.com>
2022-04-03 16:49:43 +0200

    Convert Dagger module to Kotlin

--- dagger-2/src/main/java/com/github/mrbean355/android/dagger2/HeroesModule.kt
+++ dagger-2/src/main/java/com/github/mrbean355/android/dagger2/HeroesModule.kt
@@ -1,35 +1,35 @@
-package com.github.mrbean355.android.dagger2;
+package com.github.mrbean355.android.dagger2
 
-import com.github.mrbean355.android.dagger2.data.HeroRepository;
-import com.github.mrbean355.android.dagger2.data.HeroRepositoryImpl;
-import com.github.mrbean355.android.dagger2.data.OpenDotaService;
-import com.github.mrbean355.android.dagger2.ui.ViewHeroesActivity;
-
-import javax.inject.Singleton;
-
-import dagger.Binds;
-import dagger.Module;
-import dagger.Provides;
-import dagger.android.ContributesAndroidInjector;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import com.github.mrbean355.android.dagger2.data.HeroRepository
+import com.github.mrbean355.android.dagger2.data.HeroRepositoryImpl
+import com.github.mrbean355.android.dagger2.data.OpenDotaService
+import com.github.mrbean355.android.dagger2.ui.ViewHeroesActivity
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
-public abstract class HeroesModule {
+interface HeroesModule {
 
     @ContributesAndroidInjector
-    abstract ViewHeroesActivity contributeViewHeroesActivity();
+    fun contributeViewHeroesActivity(): ViewHeroesActivity
 
     @Binds
-    abstract HeroRepository bindRepo(HeroRepositoryImpl impl);
+    fun bindRepo(impl: HeroRepositoryImpl): HeroRepository
 
-    @Provides
-    @Singleton
-    static OpenDotaService provideService() {
-        return new Retrofit.Builder()
-                .baseUrl("https://api.opendota.com/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(OpenDotaService.class);
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideService(): OpenDotaService = Retrofit.Builder()
+            .baseUrl("https://api.opendota.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
     }
 }
